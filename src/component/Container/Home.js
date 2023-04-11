f927fb3191a48596822b4b98e232dbb7951bc2e3
MdIntezarAlam <mdintezara35@gmail.com>
2023-04-11 21:24:54 +0530

    resume added

--- src/component/Container/Home.js
+++ src/component/Container/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="container-fluid pt-1 mt-1 p-4" id="home">
       <Intro />
       <div className="row pt-lg-5 px-2">
-        <h2 className="text_heading_2   pb-md-2">My Awesome Skills</h2>
+        <h2 className="text_heading_2  pb-md-2">My Awesome Skills</h2>
         <Skills />
         <h5 className="text_heading_3  py-5">My Best Projects</h5>
         {/* <Project /> */}
