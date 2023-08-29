cd93369258a0cc4b9d655d6ba643e70e0f810972
MdIntezarAlam <mdintezara35@gmail.com>
2023-08-29 15:32:13 +0530

    images updated

--- src/component/Container/RightSideIntro.js
+++ src/component/Container/RightSideIntro.js
@@ -1,17 +1,15 @@
 import { Card } from "react-bootstrap";
 import a1 from "../../image/a1.png";
 import a from "../../image/a.png";
-import n1 from "../../image/n1.jpg";
+import user from '../../image/user.jpg'
 import resume from "../Resume/resume.pdf";
-// import p1 from '../../image/p1.png'
-import port from '../../image/port.jpeg'
 const RightSideIntro = () => {
   return (
     <>
       <div class="right_img_container">
         <div className="top_img">
           <img src={a1} className="img_box img_1" alt="card_mg" />
-          <img src={port} className="img_box img_2" alt="card_mg" />
+          <img src={user} className="img_box img_2" alt="card_mg" />
         </div>
         <div className="third_img">
           <img src={a} className="img_box img_3" alt="card_mg" />
