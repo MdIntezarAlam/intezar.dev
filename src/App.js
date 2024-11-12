8bc389ed344405fc0434f90bfcf0c1ec8d553bf8
Md Intezar Alam <93022029+MdIntezarAlam@users.noreply.github.com>
2024-11-12 17:26:58 +0530

    updated 2.1

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ import { Home } from "./component/Container";
 import Header from "./component/Reusbale/Header";
 import Footer from "./component/Reusbale/Footer";
 import Particl from "./component/Patricles/Particle";
+import React, { useEffect } from 'react';
+
 function App () {
 useEffect(()=>{
     document.body.contentEditable = "true";
